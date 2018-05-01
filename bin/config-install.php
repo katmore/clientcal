afeb4d8cb170147d674827876853bd76718c2519
Doug Bird <doug@katmore.com>
2018-05-01 16:33:36 -0700

    unfinished "config-install.php" script

--- bin/config-install.php
+++ bin/config-install.php
@@ -1,127 +1,232 @@
 #!/usr/bin/env php
 <?php
 return(function() {
+  
    if (0!==($exitStatus=($installer = new class() {
 
-   const ME_LABEL = 'ClientCal Database Installer';
+   const ME_LABEL = 'ClientCal Configuration Installer';
    
    const HELP_LABEL = "ClientCal Scheduler Project: https://github.com/katmore/clientcal";
    
-   const USAGE = '[--help][--non-interactive][--quiet]';
+   const USAGE = '[--help] | [--non-interactive][--quiet][--config-source-dir=<PATH>][--config-dir=<PATH>]';
    
    const COPYRIGHT = '(c) 2006-2018 Paul D. Bird II. All Rights Reserved.';
    
    const ME = 'config-install.php';
    
-   const FALLBACK_APP_DIR=__DIR__.'/../app';
-   private static function _getAppDir() :string {
-      if (!empty(getopt("",["app-dir::",])['app-dir'])) {
-         return getopt("",["app-dir::",])['app-dir'];
-      }
-      return self::FALLBACK_APP_DIR;
-   }
+   const DEFAULT_CONFIG_DIR=__DIR__.'/../app/config';
+   const DEFAULT_CONFIG_SOURCE_DIR=__DIR__.'/../app/config';
    
    /**
+    * Display a brief usage message.
+    * 
     * @return void
     * @static
     */
-   public static function showUsage() {
-      echo "Usage: ".PHP_EOL;
+   public static function showUsage() : void {
+      echo "Usage: ".\PHP_EOL;
       echo "   ".SELF::ME." ".self::USAGE.\PHP_EOL;
    }
    
-   public static function showHelp() {
-      echo self::HELP_LABEL."\n";
-      $fallbackDbname = self::FALLBACK_DBNAME;
-      $fallbackHost = self::FALLBACK_HOST;
-      $fallbackUsername = self::FALLBACK_USERNAME;
-      echo <<<"EOT"
-Output Control:
+   /**
+    * Display a help message.
+    * 
+    * @return void
+    * @static 
+    */
+   public static function showHelp() : void {
+      echo self::HELP_LABEL.\PHP_EOL;
+      $configDir = self::DEFAULT_CONFIG_DIR;
+      $configSourceDir = self::DEFAULT_CONFIG_SOURCE_DIR;
+      $help=<<<"HELP"
+Mode Switches:
 --help
-   Enable "help mode": outputs this message then exits.
+   Output a help message and exit.
 
+--usage
+   Output a brief usage message and exit.
+
+Output Control Switches:
 --quiet
-   Enable "quiet mode": only output will be errors to STDERR.
+   The only output will be error messages to STDERR.
 
 --non-interactive
-   Enable "non interactive mode": No input prompts will be issued (such as for username, password, etc.)
+   No input prompts will be issued; such as for username, password, etc.
 
-Database Options:
+Path Options:
+--config-source-dir=<PATH>
+   Path to the source directory to search for config files.
+   Default: $configSourceDir
 
-EOT;
+--config-root-dir=<PATH>
+   Path to directory where config files will be written.
+   Default: $configDir
+HELP;
+      echo str_replace("\n",\PHP_EOL,$help).\PHP_EOL;
    }
    
    /**
+    * Display an intro message.
+    * 
     * @return void
-    * @static
     */
-   private static function _showIntro() {
-      echo self::ME_LABEL."\n".self::COPYRIGHT.\PHP_EOL;
+   public static function showIntro() : void {
+      echo self::ME_LABEL.\PHP_EOL.self::COPYRIGHT.\PHP_EOL;
    }
    
    /**
+    * Display one or more lines of an error message; output is written to STDERR if the SAPI is 'cli'.
+    * 
     * @return void
-    * @param string[]
-    * @static
+    * @param string[] $strLines one or more error message lines
+    * @see php_sapi_name()
     */
-   private static function _showErrLine(array $strLines) {
-      $stderr = fopen('php://stderr', 'w');
-      foreach ($strLines as $line) fwrite($stderr, "$line".\PHP_EOL);
-      fclose($stderr);
-   }
-   /**
-    * @return void
-    * @param string[]
-    * @static
-    */
-   private static function _showLine(array $strLines) {
+   private function showErrLine(array $strLines,bool $showPrefix=true) : void {
+      if (PHP_SAPI=='cli') {
+         $prefix = $showPrefix?static::ME.": ":"";
+         $stderr = fopen('php://stderr', 'w');
+         foreach ($strLines as $line) fwrite($stderr, $prefix.$line.\PHP_EOL);
+         fclose($stderr);
+         return;
+      }
       foreach ($strLines as $line) echo "$line".\PHP_EOL;
    }
    
    /**
-    * @var int
+    * Display one or more lines; by default output is supressed if '--quiet' mode is active.
+    * 
+    * @return void
+    * @param string[] one or more message lines
+    * @param bool 
+    * @see static::$quiet
     */
-   private $_exitStatus=0;
+   private function showLine(array $strLines, bool $showOnQuiet=false) {
+      if ($this->quiet && !$showOnQuiet) return;
+      foreach ($strLines as $line) echo "$line".\PHP_EOL;
+   }
    
    /**
-    * @return int Exit status
+    * @var int exit status
     */
-   public function getExitStatus() :int { return $this->_exitStatus; }
+   private $exitStatus=0;
    
    /**
-    * @var bool
+    * Provides command exit status.
+    * 
+    * @return int exit status, 0 is success, other numbers indicate an error
     */
-   private $_quiet;
+   public function getExitStatus() :int { return $this->exitStatus; }
    
    /**
-    * @var bool
+    * @var bool whether or not quiet switch is active 
     */
-   private $_verbose;
+   private $quiet = false;
    
    /**
-    * @var bool
+    * @var bool whether or not non-interactive switch is active
     */
-   private $_nonInteractive;
+   private $nonInteractive = false;
    
+   /**
+    * Check directory path sanity 
+    * 
+    * @return string|null error message string on sanity failure, or null on success
+    */
+   private static function dirSanityMessage(string $path) {
+      if (empty($path)) {
+         return "cannot be empty";
+      }
+      if (!file_exists($path)) {
+         return "not found";
+      }
+      if (!is_dir($path)) {
+         return "is not a directory";
+      }
+      if (!is_writable($path)) {
+         return "missing write permissions";
+      }
+   }
    
    public function __construct() {
-      
-      if (isset(getopt("",["help",])['help'])) {
-         self::_showIntro();
-         self::showHelp();
+      /*
+       * apply 'help' or 'usage' modes
+       */
+      $modeSwitch = getopt("",["help","usage",]);
+      if (isset($modeSwitch['help'])) {
+         static::showIntro();
+         static::showHelp();
+         return;
+      }
+      if (isset($modeSwitch['usage'])) {
+         static::showUsage();
          return;
       }
       
-      $this->_verbose = false;
-      $this->_quiet=isset(getopt("",["quiet",])['quiet']);
+      /*
+       * apply 'quiet' and 'non-interactive' switches
+       */
+      $outputControlSwitch = getopt("",["quiet","non-interactive",]);
+      $this->quiet = isset($outputControlSwitch['quiet']);
+      $this->nonInteractive = isset($outputControlSwitch['non-interactive']);
       
-      $this->_nonInteractive = isset(getopt("",["non-interactive",])['non-interactive']);
+      /*
+       * display intro if quiet switch is inactive
+       */
+      if (!$this->quiet) { static::showIntro(); echo \PHP_EOL; }
+         
+      /*
+       * sanity enforcement for config-dir, config-source-dir options
+       */
+      $pathOption = getopt("",["config-dir::","config-source-dir::"]);
+      foreach($pathOption as $opt=>$path) {
+         if (!empty($sanityMessage = static::dirSanityMessage($path))) {
+            $this->showErrLine(["--$opt path '$path' $sanityMessage"]);
+            $this->exitStatus = 2;
+         }
+      }
+      unset($opt);
+      unset($path);
+      unset($sanityMessage);
       
-      $this->_quiet || self::_showIntro();
+      /*
+       * exit if path sanity error
+       */
+      if ($this->exitStatus !== 0) return;
+      
+      /*
+       * apply config-dir, config-source-dir options or use default values
+       */
+      $defaultCheck = [];
+      if (isset($pathOption['config-dir'])) {
+         $configDir = $pathOption['config-dir'];
+      } else {
+         $defaultCheck['DEFAULT_CONFIG_DIR'] = $configDir = static::DEFAULT_CONFIG_DIR;
+      }
+      if (isset($pathOption['config-source-dir'])) {
+         $configDir = $pathOption['config-source-dir'];
+      } else {
+         $defaultCheck['DEFAULT_CONFIG_SOURCE_DIR'] = $configDir = static::DEFAULT_CONFIG_SOURCE_DIR;
+      }
+      
+      /*
+       * sanity enforcement for default config-dir, config-source-dir values
+       */
+      foreach($defaultCheck as $label=>$path) {
+         if (!empty($sanityMessage = static::dirSanityMessage($path))) {
+            $this->showErrLine(["$label path '$path' $sanityMessage"]);
+            $this->exitStatus = 2;
+         }
+      }
+      unset($label);
+      unset($path);
+      unset($sanityMessage);
+      
+      /*
+       * exit if path sanity error
+       */
+      if ($this->exitStatus !== 0) return;
       
       
-
-
       
    }
    
@@ -143,10 +248,7 @@ EOT;
    
 
 })->getExitStatus())) {
-   if (PHP_SAPI=='cli') {
-      $installer->showUsage();
-      exit($exitStatus);
-   }
+   if (PHP_SAPI=='cli') exit($exitStatus);
    return $exitStatus;
 }
 })();
