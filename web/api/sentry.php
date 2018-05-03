c289b511c88f0682a599bb3af0ab216ef7831558
Doug Bird <doug@katmore.com>
2018-05-02 23:04:12 -0700

    remove abandoned twbs3 template

--- web/api/sentry.php
+++
@@ -1,9 +0,0 @@
-<?php
-use clientcal\apiController;
-use clientcal\api;
-
-define("CLIENTCAL_APP_DIR",__DIR__."/../../app");
-
-require CLIENTCAL_APP_DIR . '/web-common.php';
-
-new apiController\httpInterface(new api\sentryApi);
--- web/sse/calendar.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define("CLIENTCAL_APP_DIR",__DIR__."/../../app");
-
-require CLIENTCAL_APP_DIR . '/web-common.php';
-
-require CLIENTCAL_APP_DIR . '/Resources/controller/sse/calendar.php';
-
-
