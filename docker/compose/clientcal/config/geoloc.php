8e2d3638fbdb346be10124960b0778771c2d5be8
acks <acks@jarp.localdomain>
2023-09-15 15:19:50 -0500

    blank out

--- docker/compose/clientcal/config/geoloc.php
+++
@@ -1,8 +0,0 @@
-<?php
-return (function() {
-   $config['BING_API_KEY'] = "";
-    
-   $config['GOOGLE_API_KEY'] = "";
-    
-   return $config;
-})();
--- docker/compose/clientcal/config/template.php
+++
@@ -1,6 +0,0 @@
-<?php
-return (function() {
-   $config['base_dir'] = __DIR__."/../../Resources/template/default";
-    
-   return $config;
-})();
